87cb0baed65970e207d81699020593b6cc1b3826
JuiceKo <didierjean.bastien@gmail.com>
2024-04-11 11:18:47 +0200

    Voir fonctionnel

--- app/javascript/index_corporations.js
+++ app/javascript/index_corporations.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const selectElements = document.querySelectorAll('.year-select');
-    selectElements.forEach(selectElement => {
-        selectElement.addEventListener('change', function() {
-            const corporationId = selectElement.dataset.corporationId;
-            const adherentId = selectElement.dataset.adherentId;
-            const selectedYear = this.value;
+    const voirLinks = document.querySelectorAll('.voir-link');
+
+    voirLinks.forEach(voirLink => {
+        voirLink.addEventListener('click', function(event) {
+            const adherentId = this.dataset.adherentId;
+            const selectedYear = document.querySelector(`#form-year-${adherentId}`).value;
+            const corporationId = this.dataset.corporationId;
+
             const url = `/corporations/${corporationId}/adherents/${adherentId}/formulaires/${selectedYear}`;
             window.location.href = url;
         });
