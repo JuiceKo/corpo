81f66171f1343956919058e2eb4a0afe2a6437e3
JuiceKo <didierjean.bastien@gmail.com>
2024-04-18 09:10:15 +0200

    sans le java

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,8 @@ Rails.application.routes.draw do
     resources :adherents do
       get :new_avec_adherent, on: :collection
       post :create_avec_adherent, on: :collection
-      resources :formulaires, only: [:show, :new, :create], param: :year
-      resources :formulaires, only: [:edit, :update]
-      get 'download_pdf', on: :member
+      resources :formulaires, only: [:edit, :update, :show, :new, :create]
+      get '/download_pdf', to: 'formulaires#download_pdf', as: 'download_pdf'
     end
   end
 end
